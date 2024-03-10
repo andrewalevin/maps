80d1ca1285592506880ba36e93235f3124205066
Andrew Levin <andrewlevingit@hotmail.com>
2024-03-10 06:00:26 +0300

    102

--- resizeimages.py
+++ resizeimages.py
@@ -6,6 +6,10 @@ def resize(path, size=100, allowed_extensions=None):
     if allowed_extensions is None:
         allowed_extensions = ['.png', '.jpg', '.jpeg']
     path = pathlib.Path(path)
+    print('🍎🍎🍎 ')
+    for f in path.iterdir():
+    	print(f.name)
+    	
     files = list(filter(lambda f: f.suffix in allowed_extensions and not f.stem.endswith(f'-{size}'), path.iterdir()))
     for file in files:
         thumbnail = file.with_stem(f'{file.stem}-{size}')
