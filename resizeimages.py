3e58a5dd4c22c987ac7dcbddbe39051f896467ba
Andrew Levin <andrewlevingit@hotmail.com>
2024-03-10 06:44:10 +0300

    107

--- resizeimages.py
+++ resizeimages.py
@@ -17,5 +17,61 @@ def resize(path, size=100, allowed_extensions=None):
         image.save(thumbnail.as_posix())
 
 
+def read_file(path) -> str:
+    path = pathlib.Path(path)
+    try:
+        with open(path.resolve().as_posix()) as f:
+            data = f.read()
+    except Exception as err:
+        print(f'Error with reading file!')
+        print(err)
+        print()
+    return data
+    
+
+def make_multiple_resize(path):
+    data = read_file(path)
+    for row in data.split('\n'):
+        row = row.strip()
+        parts = row.split(' ')
+        print(parts)
+        target = pathlib.Path(parts[0])
+        if not target.exists():
+            print(f'🔴 Path {target} not exists')
+            continue
+            
+        size = parts[1]
+        if not size.isnumeric:
+            print(f'🔴 size is not numeric')
+            continue
+            
+        size = int(size)
+        if size <= 0:
+            print(f'🔴 size isnot>0')
+            continue
+        
+        resize(target, size)
+        
+
+TEXT_ARGS = '''
+🔴  check arg!
+
+It should be path of filelist 
+
+resizeimageslist.txt
+
+with rows:
+target dir and size
+
+
+imgs 100
+imgs2 200
+imgs3 ervfer
+imgs0 200
+'''
+
 if __name__ == "__main__":
-    resize(sys.argv[1])
+	if len(sys.argv) == 2:
+		make_multiple_resize(sys.argv[1])
+	else:
+		print(TEXT_ARGS)
