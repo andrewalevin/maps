25674fbdc4de8ec0afa5aec3849f58bd103718c0
Andrew Levin <andrewlevingit@hotmail.com>
2024-04-08 22:02:13 +0300

    2024-04-08 22:02:12 N

---
+++ notebook/data-output/gigerevo-places.js
@@ -0,0 +1,100 @@
+const places = [{
+    'name': 'Старый ЖД мост над Москва-рекой',
+    'point': [55.614138, 36.418904],
+    'thumbnail': 'most-mars.png'
+}, {
+    'name': 'Подвестной мост на Марсе',
+    'point': [55.608452, 36.426499],
+    'thumbnail': 'most-mars-yellow.png'
+}, {
+    'name': 'Полушкинские каменоломни и природный берег',
+    'point': [55.607456, 36.530802],
+    'thumbnail': 'pokushkino-kamenolomni.png'
+}, {
+    'name': 'Подвестной мост Васильевское',
+    'point': [55.609343, 36.588827],
+    'thumbnail': 'most-vasilevo.png'
+},
+{
+    'name': 'Болота Карьера Сима',
+    'point': [55.669569, 36.712739],
+    'thumbnail': 'karery-sima.png'
+},
+{
+    'name': 'Подвестной мост в Гигерево',
+    'point': [55.705651, 36.688905],
+    'thumbnail': 'most-gigerevo.png'
+},
+{
+    'name': 'Биостанция МГУ',
+    'point': [55.700144, 36.723227],
+    'thumbnail': 'biostation.png'
+},
+{
+    'name': 'Родник на Биостанции',
+    'point': [55.707140, 36.758704],
+    'thumbnail': 'rodnik-biostation.png'
+},
+{
+    'name': 'Мост БМО',
+    'point': [55.711426, 36.790150],
+    'thumbnail': 'most-bmo.png'
+},
+{
+    'name': 'Саввино-Сторожевский монастырь',
+    'point': [55.728306, 36.816050],
+    'thumbnail': 'monastyr-savvino.png'
+},
+{
+    'name': 'Успенский собор',
+    'point': [55.733105, 36.840293],
+    'thumbnail': 'uspensky-sobor.png'
+},
+{
+    'name': 'Звенигородский городок',
+    'point': [55.735687, 36.837448],
+    'thumbnail': 'gorodok-zvenigorod.png'
+},
+{
+    'name': 'Дом Чехова',
+    'point': [55.735173, 36.846941],
+    'thumbnail': 'dom-chehova.png'
+},
+{
+    'name': 'Кофейня ДОМ',
+    'point': [55.730249, 36.852574],
+    'thumbnail': 'cofe-dom.png'
+},
+{
+    'name': 'Манеж',
+    'point': [55.729897, 36.859526],
+    'thumbnail': 'zvenigorod-manezh.png'
+},
+{
+    'name': 'Станция Звенигород',
+    'point': [55.700183, 36.881061],
+    'thumbnail': 'zvenigorod-station.png'
+},
+
+
+{
+    'name': 'Захарово усадьба',
+    'point': [55.646115, 36.969831],
+    'thumbnail': 'n.png'
+},
+{
+    'name': 'Большие Вяземы усадьба>',
+    'point': [55.628342, 36.991944],
+    'thumbnail': 'bolshy-vyazemy.png'
+},
+{
+    'name': 'Преображенская церковь в Вяземах',
+    'point': [55.627802, 36.990744],
+    'thumbnail': 'church-vyazemy.png'
+},
+
+
+
+
+
+];
